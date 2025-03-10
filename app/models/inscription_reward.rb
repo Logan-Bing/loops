29335a0d75585db9cd40927cf318e5584d31804d
logan lugez <binglei.pro@gmail.com>
2025-03-10 15:47:48 +0100

    init routes

--- app/models/inscription_reward.rb
+++ app/models/inscription_reward.rb
@@ -1,4 +1,4 @@
-class IncriptionReward < ApplicationRecord
+class InscriptionReward < ApplicationRecord
   belongs_to :inscription
   belongs_to :reward
 end
--- db/migrate/20250310143602_create_fidelity_programs.rb
+++ db/migrate/20250310143602_create_fidelity_programs.rb
@@ -1,12 +1,13 @@
 class CreateFidelityPrograms < ActiveRecord::Migration[7.1]
   def change
     create_table :fidelity_programs do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :name
       t.integer :points_per_euro
-      t.boolean :active, default: true
+      t.boolean :active
       t.datetime :created
       t.datetime :updated
-      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
