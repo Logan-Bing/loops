3a6f7aca14bb58fd8c222f83a2ce0d55d0cdf9f9
Michael Souvannarath <msouvann@gmail.com>
2025-03-11 16:08:01 +0100

    routes inscriptions et participations done

--- app/models/fidelity_program.rb
+++ app/models/fidelity_program.rb
@@ -3,7 +3,6 @@ class FidelityProgram < ApplicationRecord
   has_many :rewards, dependent: :destroy
   has_many :inscriptions, dependent: :destroy
 
-
   accepts_nested_attributes_for :rewards
 
   has_one_attached :qrcode, dependent: :destroy
