e2f1b6238dfc02dba54463266896fb19be322a55
Michael Souvannarath <msouvann@gmail.com>
2025-03-11 11:50:07 +0100

    qr code generation done

--- app/controllers/fidelity_programs_controller.rb
+++ app/controllers/fidelity_programs_controller.rb
@@ -2,19 +2,16 @@ class FidelityProgramsController < ApplicationController
   before_action :find_id, only:[:edit, :show, :update, :destroy]
 
   def index
-    @fidelity_programs = policy_scope(FidelityProgram)
     @fidelity_programs = FidelityProgram.all
   end
 
   def new
     @fidelity_program = FidelityProgram.new
-    authorize @fidelity_program
   end
 
   def create
     @fidelity_program = @fidelity_program.new(set_params)
     @fidelity_program.user_id = current_user.id
-    authorize @fidelity_program
     if @fidelity_program.save
       redirect_to fidelity_path(@fidelity_program)
     else
@@ -26,11 +23,10 @@ class FidelityProgramsController < ApplicationController
   end
 
   def edit
-    authorize @fidelity_program
   end
 
   def update
-    authorize @fidelity_program
+
     if @fidelity_program.update(set_params)
       redirect_to fidelity_path(@fidelity_program)
     else
@@ -39,7 +35,6 @@ class FidelityProgramsController < ApplicationController
   end
 
   def destroy
-    authorize @fidelity_program
     @fidelity_program.destroy
     redirect_to root_path, status: :see_other
   end
@@ -47,7 +42,7 @@ class FidelityProgramsController < ApplicationController
   private
 
   def set_params
-    params.require(:fidelity_program).permit(:name, :points_per_euro)
+    params.require(:fidelity_program).permit(:name, :points_per_euro, :qrcode)
   end
 
   def find_id
