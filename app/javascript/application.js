d2ab3f9cc21d4494a39b5de412cf4dbdab26005d
logan lugez <binglei.pro@gmail.com>
2025-03-12 17:53:59 +0100

    stimulus on nav

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-//= require cocoon
-$(document).on('turbolinks:load', function() {
-  $('.nested-fields').cocoon();
-});
-//= require jquery3
