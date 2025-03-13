92daa32d9c16a44e0a30d32f80870f5eee92084d
Manon Brault <manon_brault53@hotmail.fr>
2025-03-13 16:40:32 +0100

    Fix path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
   root to: "pages#home"
 
   resources :fidelity_programs do
