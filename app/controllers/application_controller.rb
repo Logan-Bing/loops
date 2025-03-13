a7419dd3212e33a03e5518a79dc76191c65371ea
Michael Souvannarath <msouvann@gmail.com>
2025-03-13 16:41:40 +0100

    sign up fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :store_user_location!, if: :storable_location?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  protected
 
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit({ roles: ["merchant", "customer"] }, :email, :password, :password_confirmation)
+    end
+  end
 
   private
-
 
   def after_sign_in_path_for(resource_or_scope)
     current_user.create_inscription(FidelityProgram.find(params[:fidelity_program_id])) if params[:fidelity_program_id]
