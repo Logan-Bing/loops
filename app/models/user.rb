61ea1680fa8566112c2127266536221d9ad4da1f
logan lugez <binglei.pro@gmail.com>
2025-03-14 15:44:44 +0100

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :create_inscription, if: :is_customer?
   has_one :fidelity_program
 
 
   def create_inscription(fidelity_program)
-    Inscription.create(user: self, fidelity_program: fidelity_program, created: DateTime.now.strftime("%d/%m/%Y %H:%M"))
+    Inscription.create(user: self, fidelity_program: fidelity_program)
   end
 
   def is_customer?
