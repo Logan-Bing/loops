baa0f9ef21a5cfc65e65a905c8484f71cf4a19f9
Manon Brault <manon_brault53@hotmail.fr>
2025-03-17 15:43:22 +0100

    Customize customer's rewards page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :fidelity_program
-
+  
 
   def create_inscription(fidelity_program)
     Inscription.create(user: self, fidelity_program: fidelity_program)
