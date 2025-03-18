7c4abdd8f1680daeca25290e1bd99f950e8270d8
logan lugez <binglei.pro@gmail.com>
2025-03-18 17:06:08 +0100

    jpp

--- config/initializers/google_cloud.rb
+++ config/initializers/google_cloud.rb
@@ -1,6 +1,12 @@
 require "tempfile"
+require "base64"
+
 if ENV["GOOGLE_CLOUD_KEY"]
   file = Tempfile.new(["gcp-key", ".json"])
-  file.write(ENV["GOOGLE_CLOUD_KEY"])
+
+  # Décoder le JSON depuis la variable Base64
+  file.write(Base64.decode64(ENV["GOOGLE_CLOUD_KEY"]))
   file.rewind
+
   ENV["GOOGLE_APPLICATION_CREDENTIALS"] = file.path
+end
