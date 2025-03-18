892dddbc9101dbb2c432a959c8c7038bc5bba3d7
logan lugez <binglei.pro@gmail.com>
2025-03-18 17:29:58 +0100

    saloperie

--- config/initializers/google_cloud.rb
+++ config/initializers/google_cloud.rb
@@ -1,13 +1,16 @@
-require "tempfile"
 require "base64"
+require "fileutils"
 
 if ENV["GOOGLE_CLOUD_KEY"]
-  file = Tempfile.new(["gcp-key", ".json"], encoding: 'utf-8')
+  file_path = Rails.root.join("lib", "gcp-key.json")
+
+  # S'assure que le dossier tmp existe
+  FileUtils.mkdir_p(File.dirname(file_path))
+
+  # Écrit les credentials décodés dans un fichier JSON permanent
   json_content = Base64.decode64(ENV["GOOGLE_CLOUD_KEY"]).force_encoding("UTF-8")
+  File.write(file_path, json_content, encoding: "UTF-8")
 
-  file.write(json_content)
-  file.flush
-  file.rewind
-
-  ENV["GOOGLE_APPLICATION_CREDENTIALS"] = file.path
+  # Définit la variable d'environnement avec le chemin permanent
+  ENV["GOOGLE_APPLICATION_CREDENTIALS"] = file_path.to_s
 end
