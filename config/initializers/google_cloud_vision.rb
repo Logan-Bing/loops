dde208c0a52ee716a8ce1b1129a0b449b751711e
logan lugez <binglei.pro@gmail.com>
2025-03-18 16:35:31 +0100

    jpp de ma vie

--- config/initializers/google_cloud_vision.rb
+++ config/initializers/google_cloud_vision.rb
@@ -1,5 +1,14 @@
+return if Rails.env.production? && ENV["GOOGLE_CREDENTIALS"].blank?
+
 require "google/cloud/vision"
 
-vision = Google::Cloud::Vision.image_annotator do |config|
-  config.credentials = ENV["GOOGLE_APPLICATION_CREDENTIALS"]
+if ENV["GOOGLE_CREDENTIALS"]
+  credentials_json = Base64.decode64(ENV["GOOGLE_CREDENTIALS"])
+  credentials = JSON.parse(credentials_json)
+
+  vision = Google::Cloud::Vision.image_annotator do |config|
+    config.credentials = credentials_json
+  end
+
+  Rails.application.config.google_vision_client = vision
 end
