d16cf17e55bc31261da0c5b1e5c0c62e29a8b586
logan lugez <binglei.pro@gmail.com>
2025-03-18 17:53:30 +0100

    ia

--- config/initializers/google_cloud.rb
+++ config/initializers/google_cloud.rb
@@ -8,7 +8,7 @@ if ENV["GOOGLE_CLOUD_KEY"]
   FileUtils.mkdir_p(File.dirname(file_path))
 
   # Écrit les credentials décodés dans un fichier JSON permanent
-  File.write(file_path, ENV["GOOGLE_CLOUD_KEY"])
+  File.write(file_path, ENV["GOOGLE_CLOUD_KEY"] )
 
   # Définit la variable d'environnement avec le chemin permanent
   ENV["GOOGLE_APPLICATION_CREDENTIALS"] = file_path.to_s
