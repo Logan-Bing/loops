cfe4f55a8160852e81bfa61a781ebe37e1ff04fb
logan lugez <binglei.pro@gmail.com>
2025-03-18 16:33:22 +0100

    jpp de ma vie

--- config/initializers/google_credentials.rb
+++ config/initializers/google_credentials.rb
@@ -1,13 +1,14 @@
 require 'base64'
 
-if ENV['GOOGLE_CREDENTIALS']
-  json_key = Base64.decode64(ENV['GOOGLE_CREDENTIALS'])
+# On ne charge les credentials que si on n’est pas en précompilation
+unless ARGV.include?('assets:precompile')
+  if ENV['GOOGLE_CREDENTIALS']
+    json_key = Base64.decode64(ENV['GOOGLE_CREDENTIALS'])
 
-  file_path = Rails.root.join('config', 'environments', 'winter-inquiry-453808-i5-3a146061dabc.json')
+    file_path = Rails.root.join('tmp', 'google_credentials.json')
 
-  File.open(file_path, 'w') do |f|
-    f.write(json_key)
+    File.open(file_path, 'w') { |f| f.write(json_key) }
+
+    ENV['GOOGLE_APPLICATION_CREDENTIALS'] = file_path.to_s
   end
-
-  ENV['GOOGLE_APPLICATION_CREDENTIALS'] = file_path.to_s
 end
