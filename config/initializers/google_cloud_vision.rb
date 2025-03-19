31f105a44d15cac771f51998baeff1fd6ad6978d
Manon Brault <manon_brault53@hotmail.fr>
2025-03-19 10:53:15 +0100

    Customize inscription-new.

--- config/initializers/google_cloud_vision.rb
+++ config/initializers/google_cloud_vision.rb
@@ -1,5 +1,5 @@
-require "google/cloud/vision"
+# require "google/cloud/vision"
 
-vision = Google::Cloud::Vision.image_annotator do |config|
-  config.credentials = ENV["GOOGLE_APPLICATION_CREDENTIALS"]
-end
+# vision = Google::Cloud::Vision.image_annotator do |config|
+#   config.credentials = ENV["GOOGLE_APPLICATION_CREDENTIALS"]
+# end
