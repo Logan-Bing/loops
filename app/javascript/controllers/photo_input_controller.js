d55fafa26844d25429c9f6cabfeac091e70966db
Michael Souvannarath <msouvann@gmail.com>
2025-03-19 14:38:30 +0100

    inscription show front done

---
+++ app/javascript/controllers/photo_input_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="photo-input"
+export default class extends Controller {
+  static targets = ["file", "photo", "button", "validate", "check"]
+  connect() {
+    console.log("hello")
+    this.photoTarget.addEventListener("change", this.afficherNomFichier.bind(this));
+
+
+  }
+  addPhoto() {
+    if (this.photoTarget.files.length > 0) {
+      this.validateTarget.click()
+    } else {
+      this.photoTarget.click()
+    }
+    }
+
+  afficherNomFichier() {
+    if (this.photoTarget.files.length > 0) {
+      this.fileTarget.textContent = this.photoTarget.files[0].name;
+      this.checkTarget.style.display = "block"
+      this.buttonTarget.textContent = "Valider"
+    } else {
+      this.fileTarget.textContent = "";
+    }
+  }
+}
